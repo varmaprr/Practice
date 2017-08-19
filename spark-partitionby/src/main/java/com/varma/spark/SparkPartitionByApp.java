02f792f0fbea6d5fe1fdcd570a58c43e9587c744
Varma Vetukuri <varmaprr@gmail.com>
2017-08-19 23:08:51 +0530

    Spark partition by change

---
+++ spark-partitionby/src/main/java/com/varma/spark/SparkPartitionByApp.java
@@ -0,0 +1,39 @@
+package com.varma.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scala.Tuple2;
+
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ */
+public class SparkPartitionByApp {
+
+    private static final Logger logger = LoggerFactory.getLogger(SparkPartitionByApp.class);
+
+    public static void main(String[] args) {
+
+        //Create a SparkContext to initialize
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("Word Count");
+
+        // Create a Java version of the Spark Context
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        // Load the text into a Spark RDD, which is a distributed representation of each line of text
+        JavaRDD<String> textFile = sc.textFile("src/main/resources/shakespeare.txt");
+        JavaPairRDD<String, Integer> counts = textFile
+                .flatMap(s -> Arrays.asList(s.split("[ ,]")).iterator())
+                .mapToPair(word -> new Tuple2<>(word, 1))
+                .reduceByKey((a, b) -> a + b);
+        counts.foreach(p -> System.out.println(p));
+        System.out.println("Total words: " + counts.count());
+        counts.saveAsTextFile("/tmp/shakespeareWordCount");
+
+    }
+}
