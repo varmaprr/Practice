419722a60837a1cace16205058ec04ccbd3618bd
Varma Vetukuri <varmaprr@gmail.com>
2017-11-03 09:33:32 +0530

    Toggle String

---
+++ hakerearthpractice/src/main/java/com/varma/he/FindProduct.java
@@ -0,0 +1,26 @@
+    package com.varma.he;
+
+    /**
+     * Created by varma on 11/3/2017.
+     */
+
+    import java.util.Scanner;
+
+    class FindProduct {
+
+        public static void main(String args[]) throws Exception {
+
+            Scanner s = new Scanner(System.in);
+            int N = s.nextInt();
+            int nextInt = 0;
+            double answer = 1;
+
+            for (int i = 0; i < N; i++) {
+                nextInt = s.nextInt();
+                answer = ( answer * nextInt )% (Math.pow(10,9)+7);
+            }
+
+            System.out.println((int) answer);
+        }
+    }
+
